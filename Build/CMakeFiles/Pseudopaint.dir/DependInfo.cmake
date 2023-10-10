d88dc7cf052517fc407eec05607078d79afead74
Victor Gerasimov <vitiayu@gmail.com>
2023-10-10 20:42:09 +0300

    changed project structure

---
+++ Build/CMakeFiles/Pseudopaint.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vivomisar/Documents/code/pseudopaint/Build/Pseudopaint_autogen/mocs_compilation.cpp" "CMakeFiles/Pseudopaint.dir/Pseudopaint_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/Pseudopaint.dir/Pseudopaint_autogen/mocs_compilation.cpp.o.d"
+  "/home/vivomisar/Documents/code/pseudopaint/Pseudopaint/canvas.cpp" "CMakeFiles/Pseudopaint.dir/canvas.cpp.o" "gcc" "CMakeFiles/Pseudopaint.dir/canvas.cpp.o.d"
+  "/home/vivomisar/Documents/code/pseudopaint/Pseudopaint/main.cpp" "CMakeFiles/Pseudopaint.dir/main.cpp.o" "gcc" "CMakeFiles/Pseudopaint.dir/main.cpp.o.d"
+  "/home/vivomisar/Documents/code/pseudopaint/Pseudopaint/mainwindow.cpp" "CMakeFiles/Pseudopaint.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/Pseudopaint.dir/mainwindow.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
